644ad50b1f2f038d1e5346505b7a0c5799e43b9d
Dipu K Raj <kumardipu1436@gmail.com>
2024-09-17 23:15:28 +0530

    update

---
+++ LOOPS/WAPPrintTheElementPfTheFollowingListUsingALoop.py
@@ -0,0 +1,21 @@
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])    #nums[0], nums[1], nums[2]...
+
+#     idx += 1
+
+
+
+
+
+
+
+#traverse
+
+heroes = ["ironman", "thor", "superman", "batman"]
+idx = 0
+while idx < len(heroes):
+    print(heroes[idx])
+    idx += 1

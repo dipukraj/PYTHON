0f24591092fa2209a94930bdacacb6cc08789bd9
Dipu K Raj <kumardipu1436@gmail.com>
2024-09-22 23:42:37 +0530

    update

--- LOOPS/WAPSearchForANumberXInThisTupleUsingLoop.py
+++ LOOPS/WAPSearchForANumberXInThisTupleUsingLoop.py
@@ -1,9 +1,9 @@
-nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
 
-i = 0
-while i< len(nums):
-    print(nums[i])
-    i += 1
+# i = 0
+# while i< len(nums):
+#     print(nums[i])
+#     i += 1
 
 
 
@@ -16,3 +16,18 @@ while i< len(nums):
 #     if(nums[i]==x):
 #         print("FOUND at idx", i)
 #     i += 1
+
+
+
+
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 36)
+
+x = 36
+
+i = 0
+while i<len(nums):
+    if(nums[i] == x):
+        print("Found at idx", i)
+    else:
+        print("finding...")
+    i += 1

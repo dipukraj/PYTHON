ac0022f6a36e629be884a8931276adcf98629d5d
Dipu K Raj <kumardipu1436@gmail.com>
2024-09-22 23:52:47 +0530

    update

---
+++ LOOPS/Break.py
@@ -0,0 +1,8 @@
+i = 1
+while i <=5:
+    print(i)
+    if(i==3):
+        breakk
+        i += 1
+
+        print("end of loop")
--- LOOPS/WAPSearchForANumberXInThisTupleUsingLoop.py
+++ LOOPS/WAPSearchForANumberXInThisTupleUsingLoop.py
@@ -1,9 +1,9 @@
-# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
 
-# i = 0
-# while i< len(nums):
-#     print(nums[i])
-#     i += 1
+i = 0
+while i< len(nums):
+    print(nums[i])
+    i += 1
 
 
 
@@ -20,14 +20,14 @@
 
 
 
-nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 36)
+# nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 36)
 
-x = 36
+# x = 36
 
-i = 0
-while i<len(nums):
-    if(nums[i] == x):
-        print("Found at idx", i)
-    else:
-        print("finding...")
-    i += 1
+# i = 0
+# while i<len(nums):
+#     if(nums[i] == x):
+#         print("Found at idx", i)
+#     else:
+#         print("finding...")
+#     i += 1

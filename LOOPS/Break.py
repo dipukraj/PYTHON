284185f0682de5f566c074240a8fda25a953f297
Dipu K Raj <kumardipu1436@gmail.com>
2024-09-23 23:42:13 +0530

    update

--- LOOPS/Break.py
+++ LOOPS/Break.py
@@ -1,8 +1,23 @@
-i = 1
-while i <=5:
-    print(i)
-    if(i==3):
-        breakk
-        i += 1
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i+= 1
+# print("end of loop")
 
-        print("end of loop")
+
+
+nums = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 36)
+
+x = 36
+i = 0
+while i< len(nums):
+    if(nums[i]==x):
+        print("FOUND at idx", i)
+        break
+    else:
+        print("finding...")
+        i+= 1
+
+    print("end of loop")

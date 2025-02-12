08d42a679e95e40839bc78e6dba20e43efeb30f4
Dipu K Raj <kumardipu1436@gmail.com>
2025-02-12 20:19:26 +0530

    update

--- DICTIONARY&SET/PrintTheElementsOfFollowingListUsingALoop.py
+++ DICTIONARY&SET/PrintTheElementsOfFollowingListUsingALoop.py
@@ -1,18 +1,18 @@
-# nums = [1,4,9,16,25,36,49,64,81,100]
-# idx = 0
-# while idx < len(nums):
+nums = [1,4,9,16,25,36,49,64,81,100]
+idx = 0
+while idx < len(nums):
 
-#     print(nums[idx])
+    print(nums[idx])
 
-#     idx += 1
+    idx += 1
 
 
 
 
 #Traverse
-heroes = ["ironman", "thor", "superman", "batman"]
-idx = 0
-while idx <len(heroes):
+# heroes = ["ironman", "thor", "superman", "batman"]
+# idx = 0
+# while idx <len(heroes):
 
-    print(heroes[idx])
-    idx += 1
+#     print(heroes[idx])
+#     idx += 1

89f6d471b93f16d75d8ebb5a276902ea36c4963b
Dipu K Raj <kumardipu1436@gmail.com>
2025-02-12 20:05:39 +0530

    update

--- DICTIONARY&SET/PrintTheMultiplicationTableOfaNumberN.py
+++ DICTIONARY&SET/PrintTheMultiplicationTableOfaNumberN.py
@@ -1,5 +1,17 @@
+# i = 1
+# while i<= 10:
+
+#     print(17*i)   #2,4,6,3,....
+#     i+= 1
+
+
+
+
+n=int(input("enter number"))   #kisi bhi table 2,4,5,6,10,12,19,20
+
 i = 1
 while i<= 10:
 
-    print(17*i)   #2,4,6,3,....
+    print(n*i)
     i+= 1
+    

98a1528866aa4763d8273703d23269229270af38
Dipu K Raj <kumardipu1436@gmail.com>
2025-02-22 02:36:53 +0530

    update

--- DICTIONARY&SET/Range.py
+++ DICTIONARY&SET/Range.py
@@ -9,17 +9,17 @@
 
 
 
-# seq = range(10)
+seq = range(10)
 
-# for i in seq:
-#     print(i)
-
-
-
-
-for i in range(2,10):   #range(start,stop)
-
+for i in seq:
     print(i)
+
+
+
+
+# for i in range(2,10):   #range(start,stop)
+
+#     print(i)
 
 
 

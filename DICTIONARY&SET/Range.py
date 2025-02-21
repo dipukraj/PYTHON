46948a04c57d358cd1f84f1363a82cf510c1d2b9
Dipu K Raj <kumardipu1436@gmail.com>
2025-02-22 02:33:01 +0530

    update

--- DICTIONARY&SET/Range.py
+++ DICTIONARY&SET/Range.py
@@ -1,4 +1,4 @@
-print(range(5))
+# print(range(5))
 
 
 
@@ -6,3 +6,32 @@ print(range(5))
 
 # for i in range(10):
 #     print(i)
+
+
+
+# seq = range(10)
+
+# for i in seq:
+#     print(i)
+
+
+
+
+# for i in range(2,10):   #range(start,stop)
+
+#     print(i)
+
+
+
+# for i in range(2,10,2):   #range(start,stop,step)
+
+#     print(i)
+
+
+
+# for i in range(2,100,2):  #even
+#     print(i)
+
+
+for i in range(1,101,2):  #odd
+    print(i)

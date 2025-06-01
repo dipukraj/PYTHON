7d0ec87ee474ce4be0397b1b5ef944ab55608f12
Dipu K Raj <kumardipu1436@gmail.com>
2025-06-01 14:49:16 +0530

    update

--- Phone.py
+++ Phone.py
@@ -4,6 +4,6 @@ phone_number = phonenumbers.parse("+91 7295808100")
 
 location = geocoder.description_for_number(phone_number, "en")
 
-print("Phone Number Location:", location)
+# print("Phone Number Location:", location)
  
  
